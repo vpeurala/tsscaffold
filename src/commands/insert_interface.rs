c9b70b2cb113820750f819219a9c9a6d9d0f0787
Ville Peurala <ville.peurala@op.fi>
2020-09-09 22:37:58 +0300

    Remove unnecessary code

--- src/commands/insert_interface.rs
+++ src/commands/insert_interface.rs
@@ -5,28 +5,19 @@ use std::io;
 use std::io::Write;
 
 pub fn insert_interface<W: Write>(tables: Vec<Table>, mut writer: W) -> io::Result<()> {
-    /*
-    export interface IInsertDummiesRow {
-        faceWidth: number,
-        notes: string | null,
-        asOfDate: Date
-    }
-*/
-
     for table in tables.iter() {
-        let column_names = &table.get_column_names();
-        writeln!(writer, "export interface IInsert{}Row {{", table.name.to_camel_case())?;
+        writeln!(
+            writer,
+            "export interface IInsert{}Row {{",
+            table.name.to_camel_case()
+        )?;
         for (idx, column) in table.columns.iter().enumerate() {
             writeln!(
                 writer,
                 "    {}: {}{}{}",
                 column.name.to_mixed_case(),
                 ts_type(&column.sql_type),
-                if column.is_nullable {
-                    " | null"
-                } else {
-                    ""
-                },
+                if column.is_nullable { " | null" } else { "" },
                 if idx != (table.columns.len() - 1) {
                     ","
                 } else {
@@ -34,7 +25,7 @@ pub fn insert_interface<W: Write>(tables: Vec<Table>, mut writer: W) -> io::Resu
                 }
             )?;
         }
-        write!(writer, "}}");
+        write!(writer, "}}")?;
     }
     Ok(())
 }
@@ -45,6 +36,6 @@ fn ts_type(sql_type: &String) -> String {
         "INTEGER" => String::from("number"),
         "DECIMAL" => String::from("number"),
         "DATE" => String::from("Date"),
-        otherwise => panic!("Unknown SQL type: {}", otherwise)
+        otherwise => panic!("Unknown SQL type: {}", otherwise),
     }
 }
